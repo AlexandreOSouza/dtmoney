2ca39541dc7ddeda666a7ba975e9e92002e844a4
Alexandre de Oliveira Souza <41930085+AlexandreOSouza@users.noreply.github.com>
2021-11-01 15:14:01 -0300

    feat: criando hook

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,13 +1,11 @@
-import { FormEvent, useContext, useState } from 'react'
+import { FormEvent, useState } from 'react'
 import Modal from 'react-modal'
-
-import { api } from '../../services/api'
-import { TransactionsContext } from '../../TransactionsContext'
 
 import CloseImg from'../../assets/close.svg'
 import IncomeImg from'../../assets/income.svg'
 import OutcomeImg from'../../assets/outcome.svg'
 import { Container, TransactionTypeContainer, RadioBox } from './styled'
+import { useTransactions } from '../../hooks/useTransactions'
 
 interface NewTransactionModalProps {
     isOpen: boolean;
@@ -20,7 +18,7 @@ export function NewTransactionModal({isOpen, onCloseNewTransactionModal}: NewTra
     const [value, setValue] = useState(0)
     const [ type, setType ] = useState('depoist')
 
-    const { createTransaction} = useContext(TransactionsContext);
+    const { createTransaction} = useTransactions()
 
     function clearFields() {
         setTitle('')
